9666d0af1ce5e9981a25b555c437899075ac2749
alka4747 <ak2012biu@gmail.com>
2022-11-04 23:25:02 +0200

    fixes

--- main.py
+++ main.py
@@ -21,6 +21,20 @@ def diff(new_file, old_file):
     return [new_entries_list, deleted_entries_list]
 
 
+def get_new_registered_competitors(start_list_file):
+    new_entries = []
+    competitors_list = read_xlsx_into_list(start_list_file)
+    on_site_registered_list = [ele for ele in competitors_list if ele[9] == "[הרשמה מהירה]"]
+    dotenv.load_dotenv(dotenv.find_dotenv())
+    mulka_on_site_registered_list = []
+    if os.getenv('REGISTERED_ON_SITE') != "":
+        mulka_on_site_registered_list = os.getenv('REGISTERED_ON_SITE').split(" ")
+    for entry in on_site_registered_list:
+        if str(entry[0]) not in mulka_on_site_registered_list:
+            new_entries.append(entry)
+    return new_entries
+
+
 # Read csv file
 def read_csv_into_list(file):
     if os.path.exists(file):
@@ -46,7 +60,7 @@ def read_xlsx_into_list(file):
 
 
 # Read initial guest start number from .env file
-def get_inital_guest_start_number(dotenv_file):
+def get_initial_guest_start_number(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
     if os.getenv('GUEST_START_NUMBER') is not None:
         guest_start_number = int(os.getenv('GUEST_START_NUMBER'))
@@ -130,6 +144,9 @@ def isoa_login(selenium_driver, isoa_url, user, passwd):
 
 # Download new start lists from ISOA website
 def download_new_start_lists(selenium_driver, competition_name):
+    # Remove previous start list file
+    if os.path.exists(TARGET_DOWNLOAD_DIRECTORY_PATH + '/StartList.xlsx'):
+        os.remove(TARGET_DOWNLOAD_DIRECTORY_PATH + '/StartList.xlsx')
     # Switch to isoa tab
     selenium_driver.switch_to.window(selenium_driver.window_handles[0])
     selenium_driver.refresh()
@@ -149,11 +166,7 @@ def download_new_start_lists(selenium_driver, competition_name):
     WebDriverWait(selenium_driver, CONNECTION_TIMEOUT).until(
         ec.presence_of_element_located((By.ID, "ctl00_ContentPlaceHolder1_rdbExport2Xls"))
     )
-    if os.path.exists(TARGET_DOWNLOAD_DIRECTORY_PATH + '/StartList.xlsx'):
-        if os.path.exists(TARGET_DOWNLOAD_DIRECTORY_PATH + '/StartList-old.xlsx'):
-            os.remove(TARGET_DOWNLOAD_DIRECTORY_PATH + '/StartList-old.xlsx')
-        os.rename(TARGET_DOWNLOAD_DIRECTORY_PATH + '/StartList.xlsx', TARGET_DOWNLOAD_DIRECTORY_PATH +
-                  '/StartList-old.xlsx')
+
     excel_export_button = selenium_driver.find_element_by_id("ctl00_ContentPlaceHolder1_rdbExport2Xls")
     excel_export_button.click()
     time.sleep(5)
@@ -163,94 +176,83 @@ def start_list_importer(selenium_driver, competition_name):
     download_new_start_lists(selenium_driver, competition_name)
     logging.info("Downloaded new start lists successfully")
     # Get diff list
-    if os.path.exists(TARGET_DOWNLOAD_DIRECTORY_PATH + '/StartList-old.xlsx'):
-        diff_list = diff(read_xlsx_into_list(TARGET_DOWNLOAD_DIRECTORY_PATH + '/StartList.xlsx'),
-                         read_xlsx_into_list(TARGET_DOWNLOAD_DIRECTORY_PATH + '/StartList-old.xlsx'))
-        new_entries = diff_list[0]
-        deleted_entries = diff_list[1]
-    else:
-        diff_list = diff(read_xlsx_into_list(TARGET_DOWNLOAD_DIRECTORY_PATH + '/StartList.xlsx'), [])
-        new_entries = diff_list[0]
-        deleted_entries = diff_list[1]
+    new_entries = get_new_registered_competitors(TARGET_DOWNLOAD_DIRECTORY_PATH + '/StartList.xlsx')
 
     if len(new_entries) > 0:
         for entry in new_entries:
             try:
                 register_competitor(selenium_driver, MULKA_CLOUD_URL, entry)
             except:
-                logging.exception("Unable to register " + entry[1] + " - Exiting")
-                if os.path.exists(TARGET_DOWNLOAD_DIRECTORY_PATH + '/StartList-old.xlsx'):
-                    if os.path.exists(TARGET_DOWNLOAD_DIRECTORY_PATH + '/StartList.xlsx'):
-                        os.remove(TARGET_DOWNLOAD_DIRECTORY_PATH + '/StartList.xlsx')
-                    os.rename(TARGET_DOWNLOAD_DIRECTORY_PATH + '/StartList-old.xlsx', TARGET_DOWNLOAD_DIRECTORY_PATH +
-                              '/StartList.xlsx')
+                # DELETE FROM REGISTERED LIST
+                logging.exception("Unable to register " + str(entry[0]) + " - Exiting")
                 driver.quit()
                 sys.exit(1)
             else:
-                logging.info("Registered " + entry[1] + "successfully")
+                if dotenv.get_key(dotenv_file, 'REGISTERED_ON_SITE') == "":
+                    dotenv.set_key(dotenv_file, 'REGISTERED_ON_SITE', dotenv.get_key(dotenv_file, 'REGISTERED_ON_SITE')
+                                   + str(entry[0]))
+                else:
+                    dotenv.set_key(dotenv_file, 'REGISTERED_ON_SITE',
+                                   dotenv.get_key(dotenv_file, 'REGISTERED_ON_SITE') + " " + str(entry[0]))
+                logging.info("Registered " + str(entry[0]) + " successfully")
     else:
         logging.info("no new entries")
-
-    if len(deleted_entries) > 0:
-        for entry in deleted_entries:
-            logging.info(entry[1] + "deleted his registration")
-    else:
-        logging.info("no deleted entries")
 
     dotenv.set_key(dotenv_file, 'GUEST_START_NUMBER', str(GUEST_START_NUMBER))
 
 
-# TARGET_DOWNLOAD_DIRECTORY_PATH = 'C:/temp/Orienteering'
-TARGET_DOWNLOAD_DIRECTORY_PATH = '/tmp/Orienteering'
+TARGET_DOWNLOAD_DIRECTORY_PATH = 'C:\\temp\\Orienteering'
 
-# GECKO_DRIVER_PATH = "C:/Program Files (x86)/geckodriver.exe"
+# TARGET_DOWNLOAD_DIRECTORY_PATH = '/tmp/Orienteering'
+
+GECKO_DRIVER_PATH = "C:/Program Files (x86)/geckodriver.exe"
 
 # CHROME_DRIVER_PATH = "C:/Program Files (x86)/chromedriver.exe"
 
 dotenv_file = dotenv.find_dotenv()
 
-GUEST_START_NUMBER = get_inital_guest_start_number(dotenv_file)
+GUEST_START_NUMBER = get_initial_guest_start_number(dotenv_file)
 
-# MULKA_CLOUD_URL = "https://test.mulka2.com"
+# MULKA_CLOUD_URL = "http://test.mulka2.com"
 
-MULKA_CLOUD_URL = "https://jp.mulka2.com:8443"
+MULKA_CLOUD_URL = "http://jp.mulka2.com"
 
 MULKA_EVENT_ID = "153160"
 
-MULKA_EVENT_PASSWORD = "232"
+MULKA_EVENT_PASSWORD = "450"
 
 ISOA_WEBSITE_URL = "https://nivut.org.il"
 
-COMPETITION_NAME = "בן שמן הרשמה במקום"
+COMPETITION_NAME = "יער חורשים"
 
-CONNECTION_TIMEOUT = 30
+CONNECTION_TIMEOUT = 45
 
 TIME_BETWEEN_IMPORTS = 30
 
-# profile = webdriver.FirefoxProfile()
-# profile.set_preference('browser.download.folderList', 2)
-# profile.set_preference('browser.download.manager.showWhenStarting', False)
-# profile.set_preference('browser.download.dir', TARGET_DOWNLOAD_DIRECTORY_PATH)
-# profile.set_preference('browser.helperApps.neverAsk.saveToDisk',
-#                        'text/csv; application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+profile = webdriver.FirefoxProfile()
+profile.set_preference('browser.download.folderList', 2)
+profile.set_preference('browser.download.manager.showWhenStarting', False)
+profile.set_preference('browser.download.dir', TARGET_DOWNLOAD_DIRECTORY_PATH)
+profile.set_preference('browser.helperApps.neverAsk.saveToDisk',
+                       'text/csv; application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
 
 
-# options = FirefoxOptions()
-# options.add_argument("--headless")
+options = FirefoxOptions()
+options.add_argument("--headless")
 
-chrome_prefs = {"download.default_directory": TARGET_DOWNLOAD_DIRECTORY_PATH}
-
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_experimental_option("prefs", chrome_prefs)
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
+# chrome_prefs = {"download.default_directory": TARGET_DOWNLOAD_DIRECTORY_PATH}
+#
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_experimental_option("prefs", chrome_prefs)
+# chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+# chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--no-sandbox")
+# chrome_options.add_argument("--disable-dev-shm-usage")
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
-# driver = webdriver.Firefox(firefox_profile=profile, executable_path=GECKO_DRIVER_PATH, options=options)
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+driver = webdriver.Firefox(firefox_profile=profile, executable_path=GECKO_DRIVER_PATH, options=options)
+# driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 
 
 try:
